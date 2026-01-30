cc0b43e8badb2ced1a659f35ba9ef7d4286ae933
Jannik Beyerstedt <beyerstedt@consider-it.de>
2026-01-30 10:41:19 +0100

    chore: Fix formatting

--- build.rs
+++ build.rs
@@ -6,6 +6,9 @@ fn main() {
     if let Ok(v) = env::var("DEP_OPENSSL_VERSION_NUMBER") {
         let version = u64::from_str_radix(&v, 16).unwrap();
 
-        assert!(version >= 0x3000_0000, "This crate supports openssl >= 3 only!");
+        assert!(
+            version >= 0x3000_0000,
+            "This crate supports openssl >= 3 only!"
+        );
     }
 }
